0949f41c78b5443b912630ea53bbc33f679eede5
Colin McCullough <cmccullough@microsoft.com>
2023-06-09 15:07:58 -0400

    updating with well-known configuration
    Signed-off-by: Colin McCullough <cmccullough@microsoft.com>

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,6 @@
-using BlazorBasic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OinkWeb;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
